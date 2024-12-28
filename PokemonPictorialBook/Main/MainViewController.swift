ab7d5c60034e9324cbb81a2ba1c43aa7c356a8e4
emilyj4482 <emilyj4482@gmail.com>
2024-12-28 11:15:34 +0900

    chore: project created

---
+++ PokemonPictorialBook/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PokemonPictorialBook
+//
+//  Created by EMILY on 28/12/2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
