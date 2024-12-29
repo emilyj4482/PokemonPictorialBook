dfbbd48d8a4aca25156a73b26aa1ea12fcbb6b32
emilyj4482 <emilyj4482@gmail.com>
2024-12-29 16:47:25 +0900

    feat: NetworkManager 생성

---
+++ PokemonPictorialBook/Manager/NetworkManager.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkManager.swift
+//  PokemonPictorialBook
+//
+//  Created by EMILY on 29/12/2024.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetch<T: Decodable>(url: URL) -> Single<T> {
+        return Single.create { observer in
+            AF.request(url).responseDecodable(of: T.self) { response in
+                switch response.result {
+                case .success(let value):
+                    observer(.success(value))
+                case .failure(let error):
+                    observer(.failure(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+}
