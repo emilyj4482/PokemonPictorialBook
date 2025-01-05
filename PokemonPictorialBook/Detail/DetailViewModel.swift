d8b1f7774745a710a6601c32c77c5da6cab5a914
emilyj4482 <emilyj4482@gmail.com>
2025-01-06 02:15:01 +0900

    refactor: PublishSubject를 PublishRelay로 변경

--- PokemonPictorialBook/Detail/DetailViewModel.swift
+++ PokemonPictorialBook/Detail/DetailViewModel.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 class DetailViewModel {
     
     private let disposeBag = DisposeBag()
     private let networkManager = NetworkManager.shared
     
-    let pokemonDetail = PublishSubject<Pokemon>()
+    let pokemonDetail = PublishRelay<Pokemon>()
     
     init(_ urlString: String = "https://pokeapi.co/api/v2/pokemon/132") {
         fetchPokemonDetail(urlString)
@@ -21,17 +22,16 @@ class DetailViewModel {
 
     func fetchPokemonDetail(_ urlString: String) {
         guard let url = URL(string: urlString) else {
-            pokemonDetail.onError(NetworkError.invalidURL)
             return
         }
         
         networkManager.fetch(url: url)
             .subscribe(
                 onSuccess: { [weak self] (response: Pokemon) in
-                    self?.pokemonDetail.onNext(response)
+                    self?.pokemonDetail.accept(response)
                 },
-                onFailure: { [weak self] error in
-                    self?.pokemonDetail.onError(error)
+                onFailure: { error in
+                    print(error.localizedDescription)
                 }
             )
             .disposed(by: disposeBag)
