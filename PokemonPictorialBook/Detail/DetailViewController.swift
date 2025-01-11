0a1de86a51ee30d2a34c838158c641eba93c86f0
emilyj4482 <emilyj4482@gmail.com>
2025-01-12 01:15:32 +0900

    chore: detail view를 SwiftUI로 구현하기 위헤 UIKit으로 구현한 detail view 삭제

--- PokemonPictorialBook/Detail/DetailViewController.swift
+++ PokemonPictorialBook/Detail/DetailViewController.swift
@@ -13,8 +13,6 @@ class DetailViewController: UIViewController {
     private let disposeBag = DisposeBag()
     
     var vm: DetailViewModel
-    
-    private lazy var containerView: DetailView = .init()
 
     init(vm: DetailViewModel) {
         self.vm = vm
@@ -27,9 +25,6 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addSubviews()
-        layout()
-        bind()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,35 +34,5 @@ class DetailViewController: UIViewController {
     private func setupNavigationBar() {
         navigationController?.navigationBar.isHidden = false
         navigationController?.navigationBar.tintColor = .white
-    }
-    
-    private func addSubviews() {
-        view.addSubviews([containerView])
-    }
-
-    private func layout() {
-        view.backgroundColor = .mainRed
-        
-        let safeArea = view.safeAreaLayoutGuide
-        
-        containerView.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview().inset(16)
-            $0.top.equalTo(safeArea).offset(8)
-            $0.height.equalTo(safeArea).dividedBy(1.7)
-        }
-    }
-    
-    private func bind() {
-        vm.pokemonDetail
-            .observe(on: MainScheduler.instance)
-            .subscribe(
-                onNext: { [weak self] pokemon in
-                    self?.containerView.configure(pokemon)
-                },
-                onError:  {
-                    print($0)
-                }
-            )
-            .disposed(by: disposeBag)
     }
 }
